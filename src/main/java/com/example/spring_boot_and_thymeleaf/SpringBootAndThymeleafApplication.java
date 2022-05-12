f21fc38da36068e5be7ad1d234b12dae0cf122b3
amit <amit.hasan7321@gmail.com>
2022-05-12 13:08:09 +0600

    Initializing the project on Spring Boot and Thymeleaf.

---
+++ src/main/java/com/example/spring_boot_and_thymeleaf/SpringBootAndThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_and_thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAndThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAndThymeleafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_boot_and_thymeleaf/SpringBootAndThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_and_thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAndThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
